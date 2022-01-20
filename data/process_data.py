70f2e714b0f4fa364feeb5c416617bbd3ccb6c25
Mihailo-Michael Tosic <michaeltosicde@gmail.com>
2022-01-20 21:12:51 +0000

    clean_data integrated

--- data/process_data.py
+++ data/process_data.py
@@ -1,24 +1,17 @@
-import sys
-
 # Check the versions of libraries
-# Python version
 import sys
 print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
+
 import numpy
 print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
+
 import pandas
 print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
+
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
+
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
     # load messages dataset
@@ -28,10 +21,33 @@ def load_data(messages_filepath, categories_filepath):
     df = messages.merge(categories)#, on="id")
     return(df)
 
-
 def clean_data(df):
-    pass
+    
+    print("Expanding categories column.")
+    # create a dataframe of the 36 individual category columns and name the columns properly:
+    categories = df.categories.str.split(";", expand = True) 
+    row = categories.iloc[0]     # select the first row of the categories dataframe
+    category_colnames = row.apply(lambda x: x[:len(x)-2])     # Slice string to remove last 2 characters from string
+    categories.columns = category_colnames     # rename the columns of `categories`
 
+    print("Converting category values to 0 or 1.")
+
+    #convert category values to just numbers 0 or 1:
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1:]# set each value to be the last character of the string
+        categories[column] = categories[column].astype(int)# convert column from string to numeric
+
+    print("Wrapping up: Putting together the new clean df.")
+    #wrap-up and concat
+    df.drop("categories", inplace = True, axis = "columns")# drop the original categories column from `df`
+    df = pd.concat([df, categories], axis = 1)# concatenate the original dataframe with the new `categories` dataframe
+    
+    print("Droping {} duplicates.".format(df.duplicated().sum()))
+
+    #df.drop_duplicates()
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 def save_data(df, database_filename):
     pass  
