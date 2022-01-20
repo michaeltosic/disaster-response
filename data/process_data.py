29826b6a59b2c5d33fb3293e78f401cb94726551
Mihailo-Michael Tosic <michaeltosicde@gmail.com>
2022-01-20 20:56:59 +0000

    Init Commit

---
+++ data/process_data.py
@@ -0,0 +1,67 @@
+import sys
+
+# Check the versions of libraries
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+def load_data(messages_filepath, categories_filepath):
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    ## load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories)#, on="id")
+    return(df)
+
+
+def clean_data(df):
+    pass
+
+
+def save_data(df, database_filename):
+    pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
